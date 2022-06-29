6defad667d3e20cb44e8a97d55a2ab147cd5c8a9
LocatelliBryann <bryannlocatelli@gmail.com>
2022-06-29 14:22:58 -0300

    Primeiro

---
+++ garagem/core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from core.models import Marca
+from core.models import Categoria
+
+# Register your models here.
+admin.site.register(Marca)
+admin.site.register(Categoria)
