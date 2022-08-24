9c4706252ff569db6808c11c31a2cbed22b88a52
=LocatelliBryann <=bryannlocatelli@gmail.com>
2022-08-24 17:41:35 -0300

    DRF

--- garagem/core/admin.py
+++ garagem/core/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from core.models import Marca
 from core.models import Categoria
+from core.models import Carro
+
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Carro)
